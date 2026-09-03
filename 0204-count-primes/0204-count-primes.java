3db3c5a20377dbd30ed07ac1d3fd983e05f38e3d
Onkar Londhe <olondhe726@gmail.com>
2026-09-03 17:40:14 +0530

    Time: 786 ms (9.98%), Space: 80 MB (8.33%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,7 +1,9 @@
 class Solution {
     public int countPrimes(int n) {
         boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
+        for(int i = 2 ; i<n ; i++){
+            isPrime[i] = true;
+        }
         if (n <= 2) {
             return 0;
         }
